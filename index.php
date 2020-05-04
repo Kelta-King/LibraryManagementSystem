761d57d647c7b1d2d5067955ce7ba90e4ba25f8c
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-04 07:59:47 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if($_SESSION['login']!=''){
+$_SESSION['login']='';
+}
+if(isset($_POST['login']))
+{
+  //code for captach verification
+if ($_POST["vercode"] != $_SESSION["vercode"] OR $_SESSION["vercode"]=='')  {
+        echo "<script>alert('Please enter correct verification code');</script>" ;
+    } 
+        else {
+$email=$_POST['emailid'];
+$password=md5($_POST['password']);
+$sql ="SELECT EmailId,Password,StudentId,Status FROM tblstudents WHERE EmailId=:email and Password=:password";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':email', $email, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+
+if($query->rowCount() > 0)
+{
+ foreach ($results as $result) {
+ $_SESSION['stdid']=$result->StudentId;
+if($result->Status==1)
+{
+$_SESSION['login']=$_POST['emailid'];
+echo "<script type='text/javascript'> document.location ='dashboard.php'; </script>";
+} else {
+echo "<script>alert('Admin blocked your account. Please contact admin');</script>";
+
+}
+}
+
+} 
+
+else{
+echo "<script>alert('Please enter valid details');</script>";
+}
+}
+}
+?>
