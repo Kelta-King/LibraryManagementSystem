17731b34dab247cc9ae7f3781e3e07d36423df16
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-04 08:02:54 +0530

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<div class="navbar navbar-inverse set-radius-zero" >
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" >
+
+                    <img src="assets/img/logo.png" />
+                </a>
+
+            </div>
