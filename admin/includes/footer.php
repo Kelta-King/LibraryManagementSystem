655f2c43c51a3169eb892a42fe4327b9bec3d3ff
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-05 16:07:08 +0530

    Create footer.php

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   KeltaKing's side
+                </div>
+
+            </div>
+        </div>
+    </section>
