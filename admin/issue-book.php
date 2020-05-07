4a32c300bac704c7530a363b485a792f71a066e8
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-07 17:43:32 +0530

    Create issue-book.php

---
+++ admin/issue-book.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['alogin'])==0)
+    {   
+header('location:index.php');
+}
+else{ 
+
+if(isset($_POST['issue']))
+{
+$studentid=strtoupper($_POST['studentid']);
+$bookid=$_POST['bookdetails'];
+$sql="INSERT INTO  tblissuedbookdetails(StudentID,BookId) VALUES(:studentid,:bookid)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':studentid',$studentid,PDO::PARAM_STR);
+$query->bindParam(':bookid',$bookid,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+$_SESSION['msg']="Book issued successfully";
+header('location:manage-issued-books.php');
+}
+else 
+{
+$_SESSION['error']="Something went wrong. Please try again";
+header('location:manage-issued-books.php');
+}
+
+}
+?>
