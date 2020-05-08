175385fe38f67dafffedac8f60d75365742b25c0
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-08 21:44:57 +0530

    Create manage-categories.php

---
+++ admin/manage-categories.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['alogin'])==0)
+    {   
+header('location:index.php');
+}
+else{ 
+if(isset($_GET['del']))
+{
+$id=$_GET['del'];
+$sql = "delete from tblcategory  WHERE id=:id";
+$query = $dbh->prepare($sql);
+$query -> bindParam(':id',$id, PDO::PARAM_STR);
+$query -> execute();
+$_SESSION['delmsg']="Category deleted scuccessfully ";
+header('location:manage-categories.php');
+
+}
+
+
+?>
