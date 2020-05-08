db19f7a2b6e68b068e99f4832030fe31b50e07b2
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-08 21:45:44 +0530

    Update manage-categories.php

--- admin/manage-categories.php
+++ admin/manage-categories.php
@@ -83,3 +83,97 @@ header('location:manage-categories.php');
 </div>
 </div>
 <?php } ?>
+
+         <?php if($_SESSION['delmsg']!="")
+    {?>
+<div class="col-md-6">
+<div class="alert alert-success" >
+ <strong>Success :</strong> 
+ <?php echo htmlentities($_SESSION['delmsg']);?>
+<?php echo htmlentities($_SESSION['delmsg']="");?>
+</div>
+</div>
+<?php } ?>
+
+</div>
+
+
+        </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- Advanced Tables -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                           Categories Listing
+                        </div>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Category</th>
+                                            <th>Status</th>
+                                            <th>Creation Date</th>
+                                            <th>Updation Date</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+<?php $sql = "SELECT * from  tblcategory";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{               ?>                                      
+                                        <tr class="odd gradeX">
+                                            <td class="center"><?php echo htmlentities($cnt);?></td>
+                                            <td class="center"><?php echo htmlentities($result->CategoryName);?></td>
+                                            <td class="center"><?php if($result->Status==1) {?>
+                                            <a href="#" class="btn btn-success btn-xs">Active</a>
+                                            <?php } else {?>
+                                            <a href="#" class="btn btn-danger btn-xs">Inactive</a>
+                                            <?php } ?></td>
+                                            <td class="center"><?php echo htmlentities($result->CreationDate);?></td>
+                                            <td class="center"><?php echo htmlentities($result->UpdationDate);?></td>
+                                            <td class="center">
+
+                                            <a href="edit-category.php?catid=<?php echo htmlentities($result->id);?>"><button class="btn btn-primary"><i class="fa fa-edit "></i> Edit</button> 
+                                          <a href="manage-categories.php?del=<?php echo htmlentities($result->id);?>" onclick="return confirm('Are you sure you want to delete?');"" >  <button class="btn btn-danger"><i class="fa fa-pencil"></i> Delete</button>
+                                            </td>
+                                        </tr>
+ <?php $cnt=$cnt+1;}} ?>                                      
+                                    </tbody>
+                                </table>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <!--End Advanced Tables -->
+                </div>
+            </div>
+
+
+            
+    </div>
+    </div>
+
+     <!-- CONTENT-WRAPPER SECTION END-->
+  <?php include('includes/footer.php');?>
+      <!-- FOOTER SECTION END-->
+    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY  -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+    <!-- DATATABLE SCRIPTS  -->
+    <script src="assets/js/dataTables/jquery.dataTables.js"></script>
+    <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
+      <!-- CUSTOM SCRIPTS  -->
+    <script src="assets/js/custom.js"></script>
+</body>
+</html>
+<?php } ?>
