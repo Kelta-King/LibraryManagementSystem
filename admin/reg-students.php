338089264c17b3a4c970c60a5e331ec67f465bb4
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-09 14:56:47 +0530

    Create reg-students.php

---
+++ admin/reg-students.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['alogin'])==0)
+    {   
+header('location:index.php');
+}
+else{ 
+
+// code for block student    
+if(isset($_GET['inid']))
+{
+$id=$_GET['inid'];
+$status=0;
+$sql = "update tblstudents set Status=:status  WHERE id=:id";
+$query = $dbh->prepare($sql);
+$query -> bindParam(':id',$id, PDO::PARAM_STR);
+$query -> bindParam(':status',$status, PDO::PARAM_STR);
+$query -> execute();
+header('location:reg-students.php');
+}
+
+
+
+//code for active students
+if(isset($_GET['id']))
+{
+$id=$_GET['id'];
+$status=1;
+$sql = "update tblstudents set Status=:status  WHERE id=:id";
+$query = $dbh->prepare($sql);
+$query -> bindParam(':id',$id, PDO::PARAM_STR);
+$query -> bindParam(':status',$status, PDO::PARAM_STR);
+$query -> execute();
+header('location:reg-students.php');
+}
+
+
+?>
