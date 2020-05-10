58fa21439946b1e060bc50af686f42bd8953c427
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-10 15:35:35 +0530

    Update check_availability.php

--- check_availability.php
+++ check_availability.php
@@ -14,3 +14,17 @@ if(!empty($_POST["emailid"])) {
 		$query-> execute();
 		$results = $query -> fetchAll(PDO::FETCH_OBJ);
 		$cnt=1;
+		if($query -> rowCount() > 0)
+		{
+		echo "<span style='color:red'> Email already exists .</span>";
+		 echo "<script>$('#submit').prop('disabled',true);</script>";
+		} else{
+
+			echo "<span style='color:green'> Email available for Registration .</span>";
+		 echo "<script>$('#submit').prop('disabled',false);</script>";
+		}
+	}
+}
+
+
+?>
