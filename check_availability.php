25704147b9909f9d82e1c5014b82ca0a458ead7d
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-10 15:34:49 +0530

    Update check_availability.php

--- check_availability.php
+++ check_availability.php
@@ -7,3 +7,10 @@ if(!empty($_POST["emailid"])) {
 
 		echo "error : You did not enter a valid email.";
 	}
+	else {
+		$sql ="SELECT EmailId FROM tblstudents WHERE EmailId=:email";
+		$query= $dbh -> prepare($sql);
+		$query-> bindParam(':email', $email, PDO::PARAM_STR);
+		$query-> execute();
+		$results = $query -> fetchAll(PDO::FETCH_OBJ);
+		$cnt=1;
