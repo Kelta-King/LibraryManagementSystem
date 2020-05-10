35bdfdb7462c7907f632ace1aaf05cef3c8cd20b
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-10 15:29:08 +0530

    Create adminlogin.php

---
+++ adminlogin.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if($_SESSION['alogin']!=''){
+$_SESSION['alogin']='';
+}
+if(isset($_POST['login']))
+{
+ //code for captach verification
+if ($_POST["vercode"] != $_SESSION["vercode"] OR $_SESSION["vercode"]=='')  {
+        echo "<script>alert('Incorrect verification code');</script>" ;
+    } 
+        else {
+
+$username=$_POST['username'];
+$password=md5($_POST['password']);
+$sql ="SELECT UserName,Password FROM admin WHERE UserName=:username and Password=:password";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':username', $username, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location ='admin/dashboard.php'; </script>";
+} else{
+echo "<script>alert('Invalid Details');</script>";
+}
+}
+}
+?>
