802307870f562e068938bd2a746acb57bdf49aa5
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-10 15:29:42 +0530

    Update adminlogin.php

--- adminlogin.php
+++ adminlogin.php
@@ -31,3 +31,65 @@ echo "<script>alert('Invalid Details');</script>";
 }
 }
 ?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Online Library Management System</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+</head>
+<body>
+    <!------MENU SECTION START-->
+<?php include('includes/header.php');?>
+<!-- MENU SECTION END-->
+<div class="content-wrapper">
+<div class="container">
+<div class="row pad-botm">
+<div class="col-md-12">
+<h4 class="header-line">ADMIN LOGIN FORM</h4>
+</div>
+</div>
+             
+<!--LOGIN PANEL START-->           
+<div class="row">
+<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3" >
+<div class="panel panel-info">
+<div class="panel-heading">
+ LOGIN FORM
+</div>
+<div class="panel-body">
+<form role="form" method="post">
+
+<div class="form-group">
+<label>Enter Username</label>
+<input class="form-control" type="text" name="username" autocomplete="off" required />
+</div>
+<div class="form-group">
+<label>Password</label>
+<input class="form-control" type="password" name="password" autocomplete="off" required />
+</div>
+ <div class="form-group">
+<label>Verification code : </label>
+<input type="text"  name="vercode" maxlength="5" autocomplete="off" required style="width: 150px; height: 25px;" />&nbsp;<img src="captcha.php">
+</div>  
+
+ <button type="submit" name="login" class="btn btn-info">LOGIN </button>
+</form>
+ </div>
+</div>
+</div>
+</div>  
+<!---LOGIN PABNEL END-->            
+             
