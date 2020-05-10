5ac455437a3c45c059231b846c9cfe4341f633ec
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-10 15:34:12 +0530

    Create check_availability.php

---
+++ check_availability.php
@@ -0,0 +1,9 @@
+<?php 
+require_once("includes/config.php");
+// code user email availablity
+if(!empty($_POST["emailid"])) {
+	$email= $_POST["emailid"];
+	if (filter_var($email, FILTER_VALIDATE_EMAIL)===false) {
+
+		echo "error : You did not enter a valid email.";
+	}
