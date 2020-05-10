439262f655b8496c4929e79e57f915f87c176d32
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-10 15:32:43 +0530

    Update change-password.php

--- change-password.php
+++ change-password.php
@@ -81,3 +81,50 @@ return false;
 return true;
 }
 </script>
+
+<body>
+    <!------MENU SECTION START-->
+<?php include('includes/header.php');?>
+<!-- MENU SECTION END-->
+<div class="content-wrapper">
+<div class="container">
+<div class="row pad-botm">
+<div class="col-md-12">
+<h4 class="header-line">User Change Password</h4>
+</div>
+</div>
+ <?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
+        else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>            
+<!--LOGIN PANEL START-->           
+<div class="row">
+<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3" >
+<div class="panel panel-info">
+<div class="panel-heading">
+Change Password
+</div>
+<div class="panel-body">
+<form role="form" method="post" onSubmit="return valid();" name="chngpwd">
+
+<div class="form-group">
+<label>Current Password</label>
+<input class="form-control" type="password" name="password" autocomplete="off" required  />
+</div>
+
+<div class="form-group">
+<label>Enter Password</label>
+<input class="form-control" type="password" name="newpassword" autocomplete="off" required  />
+</div>
+
+<div class="form-group">
+<label>Confirm Password </label>
+<input class="form-control"  type="password" name="confirmpassword" autocomplete="off" required  />
+</div>
+
+ <button type="submit" name="change" class="btn btn-info">Chnage </button> 
+</form>
+ </div>
+</div>
+</div>
+</div>  
+<!---LOGIN PABNEL END-->            
+             
