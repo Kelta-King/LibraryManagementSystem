2f4d6a9aa9872ab3982fe165adc91943187ca4ba
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-10 15:31:14 +0530

    Create change-password.php

---
+++ change-password.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+include('includes/config.php');
+error_reporting(0);
+if(strlen($_SESSION['login'])==0)
+    {   
+header('location:index.php');
+}
+else{ 
+if(isset($_POST['change']))
+  {
+$password=md5($_POST['password']);
+$newpassword=md5($_POST['newpassword']);
+$email=$_SESSION['login'];
+  $sql ="SELECT Password FROM tblstudents WHERE EmailId=:email and Password=:password";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':email', $email, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results = $query -> fetchAll(PDO::FETCH_OBJ);
+if($query -> rowCount() > 0)
+{
+$con="update tblstudents set Password=:newpassword where EmailId=:email";
+$chngpwd1 = $dbh->prepare($con);
+$chngpwd1-> bindParam(':email', $email, PDO::PARAM_STR);
+$chngpwd1-> bindParam(':newpassword', $newpassword, PDO::PARAM_STR);
+$chngpwd1->execute();
+$msg="Your Password succesfully changed";
+}
+else {
+$error="Your current password is wrong";  
+}
+}
+
+?>
