f93886b04f9c53c46c36c7d706ae4388655ee202
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-11 14:14:25 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -7,3 +7,21 @@ if(strlen($_SESSION['login'])==0)
 header('location:index.php');
 }
 else{?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Online Library Management System | User Dash Board</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+</head>
