98ee2880251989ddea3b77616d0c426267636785
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-11 14:14:50 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -25,3 +25,54 @@ else{?>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 
 </head>
+
+<body>
+      <!------MENU SECTION START-->
+<?php include('includes/header.php');?>
+<!-- MENU SECTION END-->
+    <div class="content-wrapper">
+         <div class="container">
+        <div class="row pad-botm">
+            <div class="col-md-12">
+                <h4 class="header-line">ADMIN DASHBOARD</h4>
+                
+                            </div>
+
+        </div>
+             
+             <div class="row">
+
+
+
+            
+                 <div class="col-md-3 col-sm-3 col-xs-6">
+                      <div class="alert alert-info back-widget-set text-center">
+                            <i class="fa fa-bars fa-5x"></i>
+<?php 
+$sid=$_SESSION['stdid'];
+$sql1 ="SELECT id from tblissuedbookdetails where StudentID=:sid";
+$query1 = $dbh -> prepare($sql1);
+$query1->bindParam(':sid',$sid,PDO::PARAM_STR);
+$query1->execute();
+$results1=$query1->fetchAll(PDO::FETCH_OBJ);
+$issuedbooks=$query1->rowCount();
+?>
+
+                            <h3><?php echo htmlentities($issuedbooks);?> </h3>
+                            Book Issued
+                        </div>
+                    </div>
+             
+               <div class="col-md-3 col-sm-3 col-xs-6">
+                      <div class="alert alert-warning back-widget-set text-center">
+                            <i class="fa fa-recycle fa-5x"></i>
+<?php 
+$rsts=0;
+$sql2 ="SELECT id from tblissuedbookdetails where StudentID=:sid and RetrunStatus=:rsts";
+$query2 = $dbh -> prepare($sql2);
+$query2->bindParam(':sid',$sid,PDO::PARAM_STR);
+$query2->bindParam(':rsts',$rsts,PDO::PARAM_STR);
+$query2->execute();
+$results2=$query2->fetchAll(PDO::FETCH_OBJ);
+$returnedbooks=$query2->rowCount();
+?>
