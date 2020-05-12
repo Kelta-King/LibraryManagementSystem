0394608b4be03c29fa32e925fef31c6abcd3cea1
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-12 14:25:05 +0530

    Create issued-books.php

---
+++ issued-books.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['login'])==0)
+    {   
+header('location:index.php');
+}
+else{ 
+if(isset($_GET['del']))
+{
+$id=$_GET['del'];
+$sql = "delete from tblbooks  WHERE id=:id";
+$query = $dbh->prepare($sql);
+$query -> bindParam(':id',$id, PDO::PARAM_STR);
+$query -> execute();
+$_SESSION['delmsg']="Category deleted scuccessfully ";
+header('location:manage-books.php');
+
+}
+
+?>
