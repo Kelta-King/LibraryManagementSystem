84ea630f74d1413798e09c7d89f0a62ff1378a52
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-12 14:26:53 +0530

    Update issued-books.php

--- issued-books.php
+++ issued-books.php
@@ -81,3 +81,40 @@ $query-> bindParam(':sid', $sid, PDO::PARAM_STR);
 $query->execute();
 $results=$query->fetchAll(PDO::FETCH_OBJ);
 $cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{               ?>                                      
+                                        <tr class="odd gradeX">
+                                            <td class="center"><?php echo htmlentities($cnt);?></td>
+                                            <td class="center"><?php echo htmlentities($result->BookName);?></td>
+                                            <td class="center"><?php echo htmlentities($result->ISBNNumber);?></td>
+                                            <td class="center"><?php echo htmlentities($result->IssuesDate);?></td>
+                                            <td class="center"><?php if($result->ReturnDate=="")
+                                            {?>
+                                            <span style="color:red">
+                                             <?php   echo htmlentities("Not Return Yet"); ?>
+                                                </span>
+                                            <?php } else {
+                                            echo htmlentities($result->ReturnDate);
+                                        }
+                                            ?></td>
+                                              <td class="center"><?php echo htmlentities($result->fine);?></td>
+                                         
+                                        </tr>
+ <?php $cnt=$cnt+1;}} ?>                                      
+                                    </tbody>
+                                </table>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <!--End Advanced Tables -->
+                </div>
+            </div>
+
+
+            
+    </div>
+    </div>
+    </div>
