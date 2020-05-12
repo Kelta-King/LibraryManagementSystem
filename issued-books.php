9c4a5108293974c5859d4416b404bfee10c14387
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-12 14:27:14 +0530

    Update issued-books.php

--- issued-books.php
+++ issued-books.php
@@ -118,3 +118,21 @@ foreach($results as $result)
     </div>
     </div>
     </div>
+<?php include('includes/footer.php');?>
+      <!-- FOOTER SECTION END-->
+    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY  -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+    <!-- DATATABLE SCRIPTS  -->
+    <script src="assets/js/dataTables/jquery.dataTables.js"></script>
+    <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
+      <!-- CUSTOM SCRIPTS  -->
+    <script src="assets/js/custom.js"></script>
+
+</body>
+</html>
+<?php 
+} 
+?>
