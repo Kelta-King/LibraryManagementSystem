2f35589133bcc3fc1347b29f9f56b9d05c84693b
Kushang Shah <52349778+Kelta-King@users.noreply.github.com>
2020-05-12 14:26:31 +0530

    Update issued-books.php

--- issued-books.php
+++ issued-books.php
@@ -73,3 +73,11 @@ header('location:manage-books.php');
                                         </tr>
                                     </thead>
                                     <tbody>
+<?php 
+$sid=$_SESSION['stdid'];
+$sql="SELECT tblbooks.BookName,tblbooks.ISBNNumber,tblissuedbookdetails.IssuesDate,tblissuedbookdetails.ReturnDate,tblissuedbookdetails.id as rid,tblissuedbookdetails.fine from  tblissuedbookdetails join tblstudents on tblstudents.StudentId=tblissuedbookdetails.StudentId join tblbooks on tblbooks.id=tblissuedbookdetails.BookId where tblstudents.StudentId=:sid order by tblissuedbookdetails.id desc";
+$query = $dbh -> prepare($sql);
+$query-> bindParam(':sid', $sid, PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
