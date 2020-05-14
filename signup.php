0b4ee7678fe4018de56b5e5002d5dbd9595fe1de
Kushang Shah <kushangshah131@gmail.com>
2020-05-14 10:30:33 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,11 @@
+<?php 
+session_start();
+include('includes/config.php');
+error_reporting(0);
+if(isset($_POST['signup']))
+{
+//code for captach verification
+if ($_POST["vercode"] != $_SESSION["vercode"] OR $_SESSION["vercode"]=='')  {
+        echo "<script>alert('Incorrect verification code');</script>" ;
+    } 
+        else {    
