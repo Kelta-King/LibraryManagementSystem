b4fc7e8d3b2427e256e954b748f8f1c9c328a9bc
Kushang Shah <kushangshah131@gmail.com>
2020-05-14 10:30:57 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -9,3 +9,36 @@ if ($_POST["vercode"] != $_SESSION["vercode"] OR $_SESSION["vercode"]=='')  {
         echo "<script>alert('Incorrect verification code');</script>" ;
     } 
         else {    
+$count_my_page = ("studentid.txt");
+$hits = file($count_my_page);
+$hits[0] ++;
+$fp = fopen($count_my_page , "w");
+fputs($fp , "$hits[0]");
+fclose($fp); 
+$StudentId= $hits[0];   
+$fname=$_POST['fullanme'];
+$mobileno=$_POST['mobileno'];
+$email=$_POST['email']; 
+$password=md5($_POST['password']); 
+$status=1;
+$sql="INSERT INTO  tblstudents(StudentId,FullName,MobileNumber,EmailId,Password,Status) VALUES(:StudentId,:fname,:mobileno,:email,:password,:status)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':StudentId',$StudentId,PDO::PARAM_STR);
+$query->bindParam(':fname',$fname,PDO::PARAM_STR);
+$query->bindParam(':mobileno',$mobileno,PDO::PARAM_STR);
+$query->bindParam(':email',$email,PDO::PARAM_STR);
+$query->bindParam(':password',$password,PDO::PARAM_STR);
+$query->bindParam(':status',$status,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+echo '<script>alert("Your Registration successfull and your student id is  "+"'.$StudentId.'")</script>';
+}
+else 
+{
+echo "<script>alert('Something went wrong. Please try again');</script>";
+}
+}
+}
+?>
