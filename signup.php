f86c39ae74cb8d6cfb66c561101e6bb58ea5cb73
Kushang Shah <kushangshah131@gmail.com>
2020-05-14 10:31:22 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -42,3 +42,34 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
 }
 }
 ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>Online Library Management System | Student Signup</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+<script type="text/javascript">
+function valid()
+{
+if(document.signup.password.value!= document.signup.confirmpassword.value)
+{
+alert("Password and Confirm Password Field do not match  !!");
+document.signup.confirmpassword.focus();
+return false;
+}
+return true;
+}
+</script>
