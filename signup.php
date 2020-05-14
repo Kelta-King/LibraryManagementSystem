c4962c04d4fcae32f2772cbb6a99066296ebd97b
Kushang Shah <kushangshah131@gmail.com>
2020-05-14 10:31:45 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -73,3 +73,20 @@ return false;
 return true;
 }
 </script>
+<script>
+function checkAvailability() {
+$("#loaderIcon").show();
+jQuery.ajax({
+url: "check_availability.php",
+data:'emailid='+$("#emailid").val(),
+type: "POST",
+success:function(data){
+$("#user-availability-status").html(data);
+$("#loaderIcon").hide();
+},
+error:function (){}
+});
+}
+</script>    
+
+</head>
