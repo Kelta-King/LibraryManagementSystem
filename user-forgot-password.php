4f61412aaed5bf0dbc8c34498aaa6c8432258b5a
Kushang Shah <kushangshah131@gmail.com>
2020-05-15 08:15:16 +0530

    Create user-forgot-password.php

---
+++ user-forgot-password.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(isset($_POST['change']))
+{
+  //code for captach verification
+if ($_POST["vercode"] != $_SESSION["vercode"] OR $_SESSION["vercode"]=='')  {
+        echo "<script>alert('Incorrect verification code');</script>" ;
+    } 
+        else {
+$email=$_POST['email'];
+$mobile=$_POST['mobile'];
+$newpassword=md5($_POST['newpassword']);
+  $sql ="SELECT EmailId FROM tblstudents WHERE EmailId=:email and MobileNumber=:mobile";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':email', $email, PDO::PARAM_STR);
+$query-> bindParam(':mobile', $mobile, PDO::PARAM_STR);
+$query-> execute();
+$results = $query -> fetchAll(PDO::FETCH_OBJ);
