1bd5bd2b480361efe821fc136bbbd45471031eca
Kushang Shah <kushangshah131@gmail.com>
2020-05-15 08:17:16 +0530

    Update user-forgot-password.php

--- user-forgot-password.php
+++ user-forgot-password.php
@@ -116,3 +116,16 @@ return true;
 </div>
 </div>
 </div>
+</div>
+    </div>
+     <!-- CONTENT-WRAPPER SECTION END-->
+ <?php include('includes/footer.php');?>
+      <!-- FOOTER SECTION END-->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+      <!-- CUSTOM SCRIPTS  -->
+    <script src="assets/js/custom.js"></script>
+
+</body>
+</html>
